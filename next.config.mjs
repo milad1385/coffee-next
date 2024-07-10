878b266a981a31af0b3ed464b0f9b2adf1f027c0
Milad salamian <miladsalami1385@gmail.com>
2024-07-10 12:59:57 -0700

    change image url

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
+        protocol: "https",
+        hostname: "coffee-next-eta.vercel.app",
         pathname: "/uploads/**",
       },
     ],
