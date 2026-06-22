d3fca02e4e2d575c504a86217b801d93cc354394
Milad salamian <miladsalami1385@gmail.com>
2026-06-23 02:23:51 +0330

    fix banner image bug

--- src/components/templates/index/banner/Banner.js
+++ src/components/templates/index/banner/Banner.js
@@ -13,7 +13,8 @@ function Banner() {
     { id: 1, image: "/images/slide1.jpg" },
     { id: 2, image: "/images/slide2.jpg" },
     { id: 3, image: "/images/slide3.jpg" },
-    { id: 4, image: "/images/slide4.jpg" },
+    // { id: 4, image: "/images/slide4.jpg" },
+    // { id: 5, image: "/images/slide5.jpg" },
   ];
   return (
     <Swiper
