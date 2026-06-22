71c515bb53673ec759afb808406ea1c58a9ace8f
Milad salamian <miladsalami1385@gmail.com>
2026-06-23 02:21:29 +0330

    fix banner component bug

--- src/components/templates/index/banner/Banner.js
+++ src/components/templates/index/banner/Banner.js
@@ -14,7 +14,6 @@ function Banner() {
     { id: 2, image: "/images/slide2.jpg" },
     { id: 3, image: "/images/slide3.jpg" },
     { id: 4, image: "/images/slide4.jpg" },
-    { id: 5, image: "/images/slide5.jpg" },
   ];
   return (
     <Swiper
