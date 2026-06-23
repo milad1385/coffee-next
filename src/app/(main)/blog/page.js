f9a4fc923e03637c45ccb4ff2c960f9ccfdddf67
Milad salamian <miladsalami1385@gmail.com>
2026-06-23 17:59:28 +0330

    add blog page

--- src/app/(main)/blog/page.js
+++ src/app/(main)/blog/page.js
@@ -1,14 +1,33 @@
 import Breadcrumb from "@/components/modules/breadcrumb/Breadcrumb";
+import Footer from "@/components/modules/footer/Footer";
 import Navbar from "@/components/modules/navbar/Navbar";
-import React from "react";
+import Pagination from "@/components/modules/product/Pagination";
+import Card from "@/components/templates/index/articles/Article";
+import styles from "@/styles/articles.module.css";
 
-function page() {
+const page = ({ searchParams }) => {
+  const page = searchParams.page || 1;
+  console.log(page);
+
   return (
     <div>
       <Navbar />
-      <Breadcrumb route="لیست مقالات" />
+      <Breadcrumb route={"اخبار و مقالات"} />
+      <main className={styles.container}>
+        <div className={styles.articles}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Pagination totalPages={10} currentPage={+page} />
+      </main>
+
+      <Footer />
     </div>
   );
-}
+};
 
 export default page;
