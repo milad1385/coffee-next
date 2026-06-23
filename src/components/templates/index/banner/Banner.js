e17379df159084a2a9a6ed4d09d2fcd07d2fd315
Milad salamian <miladsalami1385@gmail.com>
2026-06-23 15:55:28 +0330

    delete unneccery imports

--- src/components/templates/index/banner/Banner.js
+++ src/components/templates/index/banner/Banner.js
@@ -3,8 +3,6 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-
-// import required modules
 import { Navigation, Autoplay } from "swiper/modules";
 import Image from "next/image";
 
