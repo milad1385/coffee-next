8c5b39a345d3a770787faa39fd25fd3ab31dff2c
Milad salamian <miladsalami1385@gmail.com>
2026-06-24 17:51:14 +0330

    add orders component in p-user main page

--- src/components/templates/p-user/index/Orders.js
+++ src/components/templates/p-user/index/Orders.js
@@ -12,11 +12,11 @@ const Orders = () => {
           همه سفارش ها <FaArrowLeft />
         </Link>
       </div>
-      {/* <Order />
       <Order />
-      <Order /> */}
+      <Order />
+      <Order />
 
-      <p className={styles.empty}>سفارشی ثبت نشده</p>
+      {/* <p className={styles.empty}>سفارشی ثبت نشده</p> */}
     </div>
   );
 };
